f06b010b3b8ad2f8e30a950afe817781406460dd
farmani60 <farmani60@gmail.com>
2022-12-24 14:15:42 +0100

    wip

--- src/engine.py
+++ src/engine.py
@@ -49,3 +49,41 @@ def train_fn(data_loader, model, optimizer, device, scheduler):
 
         # step scheduler
         scheduler.step()
+
+
+def eval_fn(data_loader, model, device):
+    # put model in the eval mode
+    model.eval()
+    # initialize empty lists for targets and outputs
+    fin_targets = []
+    fin_outputs = []
+    # use the no_grad scope
+    # it's very important else you might
+    # run out of gpu memory
+    with torch.no_grad():
+        for data in data_loader:
+            ids = data["ids"]
+            token_type_ids = data["token_type_ids"]
+            mask = data["mask"]
+            targets = data["targets"]
+
+            # move everything to a specified device
+            ids = ids.to(device, dtype=torch.long)
+            token_type_ids = token_type_ids.to(device, dtype=torch.long)
+            mask = mask.to(device, dtype=torch.long)
+            targets = targets.to(device, dtype=torch.float)
+
+            outputs = model(
+                ids=ids,
+                mask=mask,
+                token_type_ids=token_type_ids,
+            )
+
+            # convert targets to cpu and extend the final list
+            targets = targets.cpu().detach()
+            fin_targets.extend(targets.numpy().tolist())
+            # convert outputs to cpu and extend the final list
+            outputs = torch.sigmoid(outputs).cpu().detach()
+            fin_outputs.extend(outputs.numpy().tolist())
+
+    return fin_outputs, fin_targets
